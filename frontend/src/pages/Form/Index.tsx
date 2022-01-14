0dd6dcdacdba9f46b98da8fb38ca2433083538a8
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-01-14 19:10:56 -0300

    Routes

---
+++ frontend/src/pages/Form/Index.tsx
@@ -0,0 +1,4 @@
+function Form(){
+    return(<h1>Pagina de formulario</h1>)
+}
+export default Form;
---
+++ frontend/src/pages/Listing/Index.tsx
@@ -0,0 +1,4 @@
+function Listing(){
+    return(<h1>Pagina de listagem</h1>)
+}
+export default Listing;
