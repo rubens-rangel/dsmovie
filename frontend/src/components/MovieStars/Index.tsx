3922de1606ab115f6ee511fdf03fa61b7181d473
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-01-16 19:03:31 -0300

    netlify deploy fix

--- frontend/src/components/MovieStars/Index.tsx
+++ frontend/src/components/MovieStars/Index.tsx
@@ -2,12 +2,13 @@ import { ReactComponent as StarFull} from 'assets/img/star-full.svg';
 import { ReactComponent as StarHalf} from 'assets/img/star-half.svg';
 import { ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 import './styles.css'
-import { type } from 'os';
+
 
 
 type Props = {
   score: number;
 }
+
 type StarProps = {
   fill: number;
 }
